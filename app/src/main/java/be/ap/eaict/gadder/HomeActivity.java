4b422d95b7d1c08bd83112656ab6dd699ecf9a81
Kevin Davis <kevin.davis@student.ap.be>
2017-12-20 14:54:59 +0100

    Eerste view

--- app/src/main/java/be/ap/eaict/gadder/HomeActivity.java
+++ app/src/main/java/be/ap/eaict/gadder/HomeActivity.java
@@ -2,16 +2,12 @@ package be.ap.eaict.gadder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 
-import static be.ap.eaict.gadder.R.layout.activity_main;
-
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
---
+++ app/src/main/java/be/ap/eaict/gadder/MyAdapter.java
@@ -0,0 +1,10 @@
+package be.ap.eaict.gadder;
+
+/**
+ * Created by Kevin-Laptop on 20/12/2017.
+ */
+
+public class MyAdapter {
+
+
+}
