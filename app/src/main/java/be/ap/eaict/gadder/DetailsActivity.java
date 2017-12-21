bd46287e0e7d739fd768e5ed10ac0987d55d40c9
Kevin Davis <kevin.davis@student.ap.be>
2017-12-21 11:40:02 +0100

    detailsview reset point

---
+++ app/src/main/java/be/ap/eaict/gadder/DetailsActivity.java
@@ -0,0 +1,13 @@
+package be.ap.eaict.gadder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
