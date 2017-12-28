b94bfa7f1d72844013054652908cda86f2958f5a
Ruben Nemes <ruben-nemes@msn.com>
2017-12-28 14:33:55 +0100

    datepicker toegevoegd aan when? op createactivity

--- app/src/main/java/be/ap/eaict/gadder/CreateActivity.java
+++ app/src/main/java/be/ap/eaict/gadder/CreateActivity.java
@@ -1,13 +1,53 @@
 package be.ap.eaict.gadder;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class CreateActivity extends AppCompatActivity {
 
+    public static final String TAG = "CreateActivity";
+    private EditText dateFrom;
+    private DatePickerDialog.OnDateSetListener mDateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        dateFrom = (EditText) findViewById(R.id.txtDateFrom);
+
+        dateFrom.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        CreateActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                Log.d(TAG,"onDateSet: date: " + dayOfMonth + "-" + month + "-" + year);
+            }
+        };
     }
 }
