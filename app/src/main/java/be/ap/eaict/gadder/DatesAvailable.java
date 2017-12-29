43210b7301344b1f4ab20358233dc8bf40409294
Ruben Nemes <ruben-nemes@msn.com>
2017-12-29 12:14:11 +0100

    ListAdapter,DatesAvailable classes aangemaakt, list_item layout gemaakt

---
+++ app/src/main/java/be/ap/eaict/gadder/DatesAvailable.java
@@ -0,0 +1,74 @@
+package be.ap.eaict.gadder;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+/**
+ * Created by Ruben on 29-12-2017.
+ */
+
+public class DatesAvailable implements Serializable {
+
+    private int id;
+
+    private int day;
+    private int month;
+    private int year;
+    private int hour;
+    private int minute;
+
+    public DatesAvailable(int id, int day, int month, int year, int hour, int minute) {
+        this.id = id;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public Calendar getDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(this.year, this.month, this.day, this.hour, this.minute);
+        return calendar;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+}
---
+++ app/src/main/java/be/ap/eaict/gadder/ListAdapter.java
@@ -0,0 +1,50 @@
+package be.ap.eaict.gadder;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * Created by Ruben on 29-12-2017.
+ */
+
+public class ListAdapter extends ArrayAdapter<DatesAvailable> {
+    private final Context context;
+    private final List<DatesAvailable> values;
+
+    public MyAdapter(Context context, List<DatesAvailable> values) {
+        super(context, -1, values);
+        this.context = context;
+        this.values = values;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.date_list_item, parent, false);
+
+        TextView textViewDate = (TextView) rowView.findViewById(R.id.txtDate);
+
+        //Datum String maken
+        String d = String.valueOf(values.get(position).getDay());
+        String m = String.valueOf(values.get(position).getMonth());
+        String yyyy = String.valueOf(values.get(position).getYear());
+        String date = d + "/" + m + "/" + yyyy;
+
+        //Tijd String maken
+        String hour = String.valueOf(values.get(position).getHour());
+        String minute = String.valueOf(values.get(position).getMinute());
+        String time = hour + ":" + minute;
+
+        //Velden aanpassen met setText met gegevens van de DatesAvailable repository
+        textViewDate.setText(date);
+
+        return rowView;
+    }
+}
