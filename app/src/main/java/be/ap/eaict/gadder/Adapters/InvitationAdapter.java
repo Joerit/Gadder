5ce664012446433993644a353ea72624bb7af3d9
Kevin Davis <kevin.davis@student.ap.be>
2018-01-07 18:19:52 +0100

    fucking around

---
+++ app/src/main/java/be/ap/eaict/gadder/Adapters/InvitationAdapter.java
@@ -0,0 +1,50 @@
+package be.ap.eaict.gadder.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import be.ap.eaict.gadder.DOM.Event;
+import be.ap.eaict.gadder.DOM.FBRepository;
+import be.ap.eaict.gadder.DOM.User;
+import be.ap.eaict.gadder.R;
+
+/**
+ * Created by Kevin-Laptop on 7/01/2018.
+ */
+
+public class InvitationAdapter extends ArrayAdapter<User> {
+
+    private final Context context;
+    private final List<User> values;
+
+
+    public InvitationAdapter(Context context, List<User> values) {
+        super(context, -1, values);
+        this.context = context;
+        this.values = values;
+    }
+
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.customfriendinviteslayout, parent, false);
+
+        //Insert data
+
+        TextView txtFriendNameForEvent = (TextView) rowView.findViewById(R.id.txtInviteFriendInEventName);
+
+
+        txtFriendNameForEvent.setText(values.get(position).getUsername());
+
+        //
+
+        return rowView;
+    }
+}
--- app/src/main/java/be/ap/eaict/gadder/DetailsActivity.java
+++ app/src/main/java/be/ap/eaict/gadder/DetailsActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import be.ap.eaict.gadder.Adapters.InvitationAdapter;
+import be.ap.eaict.gadder.Adapters.OverviewAdapter;
+import be.ap.eaict.gadder.DOM.FBRepository;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -11,6 +16,19 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        populateList();
+    }
+
+    /*public void onResume(){
+        super.onResume();
+        populateList();
+    }*/
+
+    public void populateList(){
+        final ListView friendInviteList = (ListView) findViewById(R.id.inviteFriendsInEventList);
+
+        InvitationAdapter invitationAdapter = new InvitationAdapter(this, FBRepository.getInstance().getUsers());
+        friendInviteList.setAdapter(invitationAdapter);
     }
 
     public void onClickEdit(View view){
