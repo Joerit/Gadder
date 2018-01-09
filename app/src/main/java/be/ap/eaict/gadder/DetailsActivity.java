6e57b6ac1dfebae0c822237db7dba016e7d483fa
Scazi <kevin-davis@hotmail.com>
2018-01-09 22:54:07 +0100

    Editview krijgt data behalve description

--- app/src/main/java/be/ap/eaict/gadder/DetailsActivity.java
+++ app/src/main/java/be/ap/eaict/gadder/DetailsActivity.java
@@ -89,10 +89,6 @@ public class DetailsActivity extends AppCompatActivity
     }
 
     public void openEditActivity(){
-        String txtWhat = new String();
-        String txtHow = new String();
-        String txtWhere = new String();
-        String txtDate = new String();
 
         Intent intent = new Intent(DetailsActivity.this, EditActivity.class);
         intent.putExtra("eventId", event.getId());
