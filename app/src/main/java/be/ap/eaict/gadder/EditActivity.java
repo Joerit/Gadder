e0449e999efe2cdd1a7b43c9e49b20afcceaac8f
Scazi <kevin-davis@hotmail.com>
2018-01-09 23:48:51 +0100

    Edit werkende

--- app/src/main/java/be/ap/eaict/gadder/EditActivity.java
+++ app/src/main/java/be/ap/eaict/gadder/EditActivity.java
@@ -6,11 +6,13 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -28,6 +30,7 @@ public class EditActivity extends AppCompatActivity {
     private Button btnDates;
     private List<String> dates;
     private int _id;
+    private Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +79,14 @@ public class EditActivity extends AppCompatActivity {
                 startActivityForResult(intent,1);
             }
         });
+
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                editEvent();
+            }
+        } );
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -105,6 +116,45 @@ public class EditActivity extends AppCompatActivity {
                 txtLocation.setText(event.getPlace(), TextView.BufferType.EDITABLE);
                 txtEventDate.setText(event.getEventDate(), TextView.BufferType.EDITABLE);
             }
+        }
+    }
+
+    private void editEvent(){
+        EditText txtEventName = (EditText)findViewById(R.id.txtEventName);
+        EditText txtDescription = (EditText)findViewById(R.id.txtDescription);
+        EditText txtLocation = (EditText)findViewById(R.id.txtLocation);
+        EditText txtEventDate = (EditText)findViewById(R.id.txtEventDate);
+
+        //check validity
+        String strName = txtEventName.getText().toString();
+        String strLoc = txtLocation.getText().toString();
+        String strDesc = txtDescription.getText().toString();
+
+        if(strName.equals("") || strLoc.equals("") || strDesc.equals("")){
+            Toast.makeText(EditActivity.this, "please fill out everything", Toast.LENGTH_SHORT).show();
+        }
+        else if (dates.size() == 0){
+            Toast.makeText(EditActivity.this, "Please add at least one date", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Event newEvent = new Event(
+                    _id,
+                    txtEventName.getText().toString(),
+                    txtLocation.getText().toString(),
+                    txtDescription.getText().toString(),
+                    GlobalData.currentUser.getId(),
+                    txtEventDate.getText().toString(),
+                    dates);
+            newEvent.addInvitedUser(GlobalData.currentUser);
+
+            FBRepository repo = FBRepository.getInstance();
+            //first create event to get ID
+            repo.createOrUpdateEvent(newEvent);
+            Log.d("ONCLICK", "newEventID = " + newEvent.getId());
+            //GlobalData.currentUser.addEvent(newEvent);
+            //repo.createOrUpdateUser(GlobalData.currentUser);
+
+            finish();
         }
     }
 }
